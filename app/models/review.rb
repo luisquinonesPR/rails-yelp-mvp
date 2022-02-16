e557cee3227e5a17f1561786a4b67a5176580d1f
Luis Quinones <luis.quinones9@upr.edu>
2022-02-16 12:58:01 +0100

    controllers, routes and db

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :content, length: { minimum: 5 }
+  validates :rating, inclusion: { in: 0..5 }
+  validates :rating, numericality: {only_integer: true}
+end
